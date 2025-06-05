9b4cd833326a273651fafcd7d95f428fdeba7148
Sharanpreet Singh <sharanpreet.singh@magicsw.com>
2025-06-05 14:19:31 +0530

    adding chat bot

--- App.tsx
+++ App.tsx
@@ -5,12 +5,15 @@ import WelcomeScreen from './src/screen/Auth/WelcomScreen'
 import RegisterScreen from './src/screen/Auth/RegisterScreen'
 import { NavigationContainer } from '@react-navigation/native'
 import BottomTabNavigation from './src/naviagtion/BottomTabNavigation'
+import ChatScreen from './src/screen/ChatScreen'
 
 const App = () => {
   return (
-    <NavigationContainer>
-       <BottomTabNavigation />
-    </NavigationContainer>
+    // <NavigationContainer>
+    //    <BottomTabNavigation />
+    // </NavigationContainer>
+
+    <ChatScreen />
  
   
   )
--- src/screen/ChatScreen.tsx
+++ src/screen/ChatScreen.tsx
@@ -1,14 +1,75 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { Image, StyleSheet, Text, TextInput, Touchable, TouchableOpacity, View } from 'react-native'
+import React, { useState } from 'react'
+import { GoogleGenAI } from "@google/genai";
 
 const ChatScreen = () => {
-  return (
-    <View>
-      <Text>ChatScreen</Text>
-    </View>
-  )
+    const ai = new GoogleGenAI({ apiKey: "AIzaSyA9fhcl6gcerhv_qV7feccK3SAQ7Rn9g6E" });
+    const [inputMessage, setInputMessage] = useState('')
+    const [responseresult , setResponseresult ] = useState('')
+    
+
+    const genratePrompt = async () => {
+        const response = await ai.models.generateContent({
+            model: 'gemini-2.0-flash',
+            contents: inputMessage,
+        })
+
+        console.log(response.text),
+        setResponseresult(response.text)
+    }
+   
+
+    const handleSetText = (text) => {
+        setInputMessage(text)
+    }
+    return (
+        <View style={styles.maincontainer}>
+            <View style={styles.header}>
+                <Text> {responseresult} </Text>
+            </View>
+            <View style={styles.chatboxholder}>
+                <TextInput placeholder='Text Here ' editable={true} style={styles.inputfield} onChangeText={handleSetText} value= {inputMessage} />
+                <TouchableOpacity style={styles.buttonwraper} onPress={genratePrompt}>
+                    <Image source={require('../assets/icon/paperplane.png')} style={styles.sendbutton} />
+                </TouchableOpacity>
+            </View>
+        </View>
+    )
 }
 
 export default ChatScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    maincontainer: {
+        flex: 1,
+        justifyContent: 'space-between'
+    },
+    inputfield: {
+        height: 40,
+        borderColor: 'gray',
+        borderWidth: 1,
+        marginBottom: 20,
+        margin: 10,
+        padding: 10,
+        borderRadius: 20,
+        width: 300
+
+    },
+    chatboxholder: {
+        flexDirection: 'row'
+    },
+    sendbutton: {
+        height: 20,
+        width: 20,
+        tintColor: 'blue',
+
+
+    },
+    buttonwraper: {
+        marginTop: 20,
+    },
+    header: {
+        justifyContent: 'center',
+        flex: 1
+    }
+})
